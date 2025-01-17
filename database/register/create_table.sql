d59e7432ff561afae433724d39fc78d7c39a475a
unknown <salmanaalfariz01@gmail.com>
2025-01-17 09:19:53 +0700

    test commit

---
+++ database/register/create_table.sql
@@ -0,0 +1,29 @@
+-- Tabel for Divisi
+CREATE TABLE divisions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Tabel for Jabatan (Position)
+CREATE TABLE positions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    division_id INT NOT NULL,
+    FOREIGN KEY (division_id) REFERENCES divisions(id) ON DELETE CASCADE
+);
+
+
+-- Tabel for User Register
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    position_id INT NOT NULL,
+    division_id INT NOT NULL,
+    phone int unique not null,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    join_date DATE NOT NULL,
+    status ENUM('active', 'inactive') DEFAULT 'active',
+    FOREIGN KEY (position_id) REFERENCES positions(id) ON DELETE CASCADE,
+    FOREIGN KEY (division_id) REFERENCES divisions(id) ON DELETE CASCADE
+);
